664110db4cb0c054026f0c9b0465205bfc405cc8
MrAdiaraaa-087 <87253374+MrAdiaraaa-087@users.noreply.github.com>
2021-07-11 14:42:43 +0700

    Add files via upload

---
+++ Tampilanw.sh
@@ -0,0 +1,13 @@
+clear
+
+figlet TERMINAL | lolcat
+echo
+sleep 2
+echo '÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷' | lolcat
+echo '[+]AUTHOR : MR Adiaraaa           [+]' | lolcat
+echo '[+]TEAM   : Cyber Attacker        [+]' | lolcat
+echo '[+]WA     : 081546772792          [+]' | lolcat
+echo '÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷' | lolcat
+echo
+echo
+echo
